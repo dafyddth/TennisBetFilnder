93f9f292955cf5830a76810f98db045e223983c6
dafyddth <dafyddth@googlemail.com>
2024-12-14 13:43:57 +0000

    Remove extra prints

--- Main.py
+++ Main.py
@@ -1,16 +1,17 @@
 from datetime import datetime
-
 from OddsLookup import OddsHolder
 import Betfair
 import functions
 import time
-
 from functions import ideal_back_stake
 
+
 my_odds_holder = OddsHolder(r"C:\Users\dafyd\PycharmProjects\TennisBetfinder\\", r"OddsLookupTrends.csv")
-max_iterations = 144
+max_iterations = 3000
 iterations = 0
-lay_stake = 5
+required_liability = 0.5
+
+
 while iterations < max_iterations:
     #Get the list of bets
     bet_list = Betfair.get_betfair_data()
@@ -27,17 +28,26 @@ while iterations < max_iterations:
             match_start_time = bet[2]
             player = bet[3]
             lay_odds = bet[4]
+            lay_stake = round(required_liability / (lay_odds - 1), 2)
             back_odds = my_odds_holder.get_back_odds(str(bet[4]))
-            back_stake = round(ideal_back_stake(lay_odds, back_odds, lay_stake),2)
+            back_stake = round(ideal_back_stake(lay_odds, back_odds, lay_stake), 2)
+
+            # Format the values as currency
+            formatted_lay_stake = f"£{lay_stake:.2f}"
+            formatted_back_stake = f"£{back_stake:.2f}"
+            formatted_liability = f"£{required_liability:.2f}"
+
             text_message = (f"Tennis Bet: \n"
                             f"Match: {event_name}\n"
                             f"Start time: {match_start_time}\n"
                             f"Player: {player.upper()}\n"
                             f"Lay odds: {lay_odds}\n"
+                            f"Liability: {formatted_liability}\n"
+                            f"Lay stake: {formatted_lay_stake}\n"
                             f"Back odds: {back_odds}\n"
-                            f"Back stake: {back_stake}")
+                            f"Back stake: {formatted_back_stake}")
 
-            #print(text_message)
+            # print(text_message)
             functions.send_bet_notification(text_message)
 
             functions.record_bet(bet[0])
